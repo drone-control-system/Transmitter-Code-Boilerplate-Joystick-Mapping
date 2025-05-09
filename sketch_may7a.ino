a1f90d0d2f13cd26121b434e53f1084dbff14fd1
Kira <78575847+Syren0914@users.noreply.github.com>
2025-05-09 12:15:47 -0400

    update

--- sketch_may7a.ino
+++ sketch_may7a.ino
@@ -15,7 +15,6 @@ const int yawPin = A1;
 const int pitchPin = A3;
 const int rollPin = A4;
 
-
 void setup() {
   Serial.begin(9600);
 }
@@ -41,5 +40,6 @@ void loop() {
   Serial.print(" | Pitch: "); Serial.print(packet.pitch);
   Serial.print(" | Roll: "); Serial.println(packet.roll);
 
+  // Send packet over Serial
   delay(500);
 }
